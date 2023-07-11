4edb9e59210a51edd41bda0d1eda3704a23c3095
Scott DeVoss <scottdevoss@yahoo.com>
2023-07-11 12:58:22 -0600

    add methods

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,19 @@ integers = [0, 1, 2, 3]
 floats = [1.2, 7.3, 10.9, 8.5]
 booleans = [true, false, true, true]
 
+#the shift method removes the first element of the array and returns that removed element.
+names.shift
+puts names 
+
+#the unshift method is used to fill the array with elements that will begin at the front of the array.
+integers.unshift
+puts integers
+
+#the push method is used to add elements at the end of an array.
+floats.push
+puts floats
+
+#the pop method is used to remove the last element of an array and returns that removed element. 
+booleans.pop
+puts booleans
+
